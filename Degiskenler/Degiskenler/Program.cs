360a08b034b3c45df621336a33446d6850d2785a
denizdogruel12 <denizdogruel20@gmail.com>
2025-03-14 07:57:47 +0300

    Add files via upload

---
+++ Degiskenler/Degiskenler/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Degiskenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Degisken türü Degisken adi=deger;
+            //string mesaj = "merhaba nasılsın";
+            //Console.WriteLine(mesaj);
+            //Console.WriteLine(mesaj);
+            //Console.WriteLine(mesaj);
+            //Console.WriteLine(mesaj);
+            //Console.WriteLine(mesaj);
+            /*
+             
+             string
+            string isim="Ahmet";
+            int sayi=10;
+            double ondaliklisayi=10.5;
+            char ilkharf='s';
+            
+
+             
+             */
+            //string isim = "Ahmet";
+            //Console.WriteLine(isim);
+            //int sayi = 10;
+            //Console.WriteLine(sayi);
+            //double ondaliklisayi = 10.5;
+            //Console.WriteLine(ondaliklisayi);
+            //char ilkharf = 's';
+            //Console.WriteLine(ilkharf);
+
+            //string ad = "Ali";
+            //string soyad = "Yalçın";
+            //string yas = "30";
+            //Console.WriteLine("**** kişi karti **** ");
+            //Console.WriteLine("Adınız: " + ad);
+            //Console.WriteLine("Soyadınız: "+soyad);
+            //Console.WriteLine("yas: " + yas);
+            //Console.Write("kullanici adi Giriniz: ");
+            //string kullaniciadi = Console.ReadLine();
+            //Console.Write("sifre Giriniz: ");
+            //string sifre = Console.ReadLine();
+            //Console.WriteLine("kullanıcı adınız: " + kullaniciadi);
+            //Console.WriteLine("Sifreniz: " + sifre);
+            //Console.Write("dogumyilinizi Giriniz :");
+            //int dogumyili = Convert.ToInt32(Console.ReadLine());
+            //int simdikiyil = 2025;
+            //int yas = simdikiyil - dogumyili;
+            //Console.WriteLine("yas: " + yas);
+            //Console.Write("Boyunuz: ");
+            //double boy = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Boyunuz: " + boy);
+            Console.Write("Cinsiyet Giriniz: ");
+            char cinsiyet = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("cinsiyet: " + cinsiyet);
+
+        }
+    }
+}
